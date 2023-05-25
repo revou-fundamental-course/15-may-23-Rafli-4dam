b74c6002904cf80e39abb3700da5aadf9286dad9
Rafli-4dam <rafli4dam@gmail.com>
2023-05-25 11:40:21 +0800

    adding square area calculation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
+//Fungsi hitung luas
 function hitungLuas() {
-    var sisiLuas = document.getElementById("sisi-luas").value;
-    // console.log(sisiLuas);
     
+    //Input Sisi Persegi
+    var sisiLuas = document.getElementById("sisi-luas").value;
+    
+    //Reveal perhitungan seteleh sisi persegi diinput
     var resultLuas = document.getElementsByClassName("result-luas");
     for (var i=0;i<resultLuas.length;i+=1){
         resultLuas[i].style.display = 'block';
     }
+    //Memasukkan bilangan sisi persegi yang di input untuk ditampilkan
     document.getElementById("perhitungan-luas").innerText = sisiLuas;
     document.getElementById("perhitungan-luas-2").innerText = sisiLuas;
-    // console.log(perhitunganLuas);
-    // for (var i=0;i<perhitunganLuas.length;i+=1){
-        //     perhitunganLuas[i].innerText = sisiLuas;
-        // }
+    
+    //Menghitung luas persegi
+    let x = document.getElementById("sisi-luas").value;
+    let y = document.getElementById("sisi-luas").value;
+    let z = x * y;
+    document.getElementById("output-luas").innerHTML = z;
+
     }
+
+    //Perintah Reset Luas Persegi
     function resetLuas() {
     document.getElementById("perhitungan-luas").innerText = "";
     document.getElementById("perhitungan-luas-2").innerText = "";
